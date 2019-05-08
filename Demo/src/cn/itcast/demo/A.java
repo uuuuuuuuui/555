028cfca7039a92ab6a0a5b4788c236f5f2e9d013
sublun <sublun@126.com>
2019-05-08 10:22:10 +0800

    2

--- Demo/src/cn/itcast/demo/A.java
+++ Demo/src/cn/itcast/demo/A.java
@@ -4,6 +4,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("我是A");
         System.out.println("我是A123");
+        System.out.println("我是A123");
 
     }
 }
